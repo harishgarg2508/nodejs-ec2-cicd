fdf9a081d01c32af4d950164424a8fba0a90c978
Harish Garg <rishusharma123321@gmail.com>
2025-04-10 10:21:21 +0530

    final

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 
 app.get('/', (req, res) => {
   res.send('Data is live go to /users to view the data!');
