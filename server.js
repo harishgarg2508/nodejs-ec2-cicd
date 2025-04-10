ec3f9b1088a76a2b5331bf8fbb12326815de53b3
Harish Garg <rishusharma123321@gmail.com>
2025-04-10 06:24:02 +0530

    done

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Data is live go to /users to view the data!');
+});
+
+app.get('/users', (req, res) => {
+  fs.readFile('./users.json', 'utf8', (err, data) => {
+    if (err) return res.status(500).send('Error reading file');
+    const users = JSON.parse(data);
+    res.json(users);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running at http://localhost:${PORT}`);
+});
